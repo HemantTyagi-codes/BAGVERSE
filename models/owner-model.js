b8791aa88369a0d246b358cadf0799b07f568eed
HemantTyagi-codes <tyagihemant550@gmail.com>
2026-06-03 22:34:15 +0530

    3 june - added separation of concerns in models and created owner model

---
+++ models/owner-model.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose');
+
+
+
+const ownerSchema=mongoose.Schema({
+    fullname:{
+        type:String,
+        minLength:3,
+        trim:true,},
+
+    email:String,
+
+    password:String,
+
+
+    products:{
+        type:Array,
+        default:[]
+    },
+    
+    
+    
+    picture:String,
+    gstin:String,
+
+
+});
+
+modeule.exports=mongoose.model("owner",ownerSchema);
